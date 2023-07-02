2770792dcde1f426b190a84abb948c40d7c94f38
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-07-02 14:55:22 -0600

    Set up types, header, and init state

---
+++ src/types.ts
@@ -0,0 +1,26 @@
+export interface User {
+    id: number;
+    userId: string;
+    username: string;
+    email: string;
+    password: string;
+}
+
+export interface Quote {
+    id: number;
+    quoteId: string;
+    quote: string;
+    author: string;
+    category: string;
+    creatorId: string;
+}
+
+export interface Favorite {
+    id: number;
+    userId: string;
+    quoteId: string;
+}
+
+export interface ChildrenProps {
+    children: React.ReactNode;
+}
