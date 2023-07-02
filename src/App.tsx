8b7f7462654fba6d016c81cbd90d0364760e2f57
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-07-02 14:55:34 -0600

    Set up types, header, and init state

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,21 @@
-import './App.css'
+import './App.css';
+import { useState } from 'react';
 import AccountForms from './components/AccountForms/AccountForms';
 import CreateQuote from './components/CreateQuote/CreateQuote';
 import Favorites from './components/Favorites/Favorites';
 import Header from './components/Header/Header';
 import Home from './components/Home/Home';
-import { useState } from 'react';
-
 
 function App() {
-  const [pageView, setPageView] = useState('home');
-  
+  const [pageView, setPageView] = useState('home')
+
   return (
     <>
-      <Header />
+      <Header page={pageView} setPageView={setPageView} />
       {pageView === 'home' && <Home />}
       {pageView === 'favorites' && <Favorites />}
       {pageView === 'account' && <AccountForms />}
       {pageView === 'create' && <CreateQuote />}
-
     </>
   )
 }
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,4 +1,15 @@
-export default function Header() {
+type HeaderProps = {
+    page: string,
+    setPageView: (page: string) => void
+}
+
+export default function Header({ page, setPageView }: HeaderProps) {
+    console.log(page)
+    
+    const handlePageChange = (page: string) => {
+        setPageView(page)
+    }
+    
     return (
         <>
             <h1>SoulQuotes Header</h1>
