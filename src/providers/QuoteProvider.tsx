388c10263ce951a59b4b1a8072ad043df389b39a
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-07-06 16:41:14 -0600

    Remove extra code from provider

--- src/providers/QuoteProvider.tsx
+++ src/providers/QuoteProvider.tsx
@@ -4,7 +4,6 @@ import { getQuotes } from "../api/api-actions";
 
 export type QuoteContextType = {
   quotes: Quote[] | [];
-  changeActiveQuotes: (quotesArr: []) => void;
 };
 
 export const QuoteContext = createContext<QuoteContextType | object>({});
@@ -13,25 +12,14 @@ export const QuoteProvider = ({ children }: ChildrenProps) => {
   const [quotes, setQuotes] = useState([]);
 
 
-  const changeActiveQuotes = (quotesArr: []) => {
-    const quoteOneIdx = Math.floor(Math.random() * quotesArr.length);
-    const quoteTwoIdx =
-      quoteOneIdx + 10 > quotesArr.length ? quoteOneIdx - 10 : quoteOneIdx + 10;
-    const quoteThreeIdx =
-      quoteOneIdx + 20 > quotesArr.length ? quoteOneIdx - 20 : quoteOneIdx + 20;
-    
-  };
-
   useEffect(() => {
     getQuotes().then((quotes) => {
       setQuotes(quotes);
-      changeActiveQuotes(quotes);
     });
   }, []);
 
   const providerValue = {
     quotes,
-    changeActiveQuotes,
   };
 
   return (
