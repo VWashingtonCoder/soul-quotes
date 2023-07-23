1ce9ffaba3853f887c12fcd7457062158ea1a24a
Andre Washington <kuraiokamiblaze@gmail.com>
2023-07-23 10:00:31 -0600

    Style create quote form

--- src/components/CreateQuote/CreateQuote.tsx
+++ src/components/CreateQuote/CreateQuote.tsx
@@ -1,7 +1,7 @@
 import { CategorySelect } from "../CategorySelect/CategorySelect";
+import "./CreateQuote.css";
 
 export default function CreateQuote() {
-
     return (
         <section className="page create">
             <h2 className="page-title">Create Your Own Quote</h2>
@@ -9,28 +9,34 @@ export default function CreateQuote() {
             <form id="createForm">
                 <div className="form-input-container flex-align-center">
                     <label className="input-label">Quote*: </label>
-                    <textarea 
-                        className="form-input quote" 
+                    <textarea
+                        className="form-input quote"
                         name="quote"
-                        cols={50} 
-                        rows={4} 
+                        cols={30}
+                        rows={3}
                         placeholder="To Make Or Not Make A Quote...That Is The Question."
-                        required 
+                        required
                     />
                 </div>
 
-                <div className="form-input-container">
+                <div className="form-input-container flex-align-center">
                     <label className="input-label">Author*: </label>
-                    <input className="form-input author" name="author" type="text" required />
+                    <input
+                        className="form-input author"
+                        name="author"
+                        type="text"
+                        placeholder="Author Name"
+                        required
+                    />
                 </div>
 
-                <div className="form-input-container">
+                <div className="form-input-container flex-align-center">
                     <label className="input-label">Category*: </label>
                     <CategorySelect />
                 </div>
 
                 <input className="submit-btn" type="submit" />
-                
+
             </form>
         </section>
     )
