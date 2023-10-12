5e6b22f9e2273f216a68ecefeec665fedea09555
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-10-11 18:14:39 -0600

    Add in api actions for database

--- src/api.ts
+++ src/api.ts
@@ -1,2 +1,66 @@
+import { Favorite, Quote, User } from "./types";
+
 const API_URL = "http://localhost:3000";
+
+export const getAllUsers = async () => {
+    const response = await fetch(`${API_URL}/users`);
+    return response.json();
+};
+
+export const addUser = async (user: User) => {
+    const response = await fetch(`${API_URL}/users`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify(user),
+    });
+    return response.status;
+};
+
+export const getAllQuotes = async () => {
+    const response = await fetch(`${API_URL}/quotes`);
+    return response.json();
+};
+
+export const addQuote = async (quote: Quote) => {
+    const response = await fetch(`${API_URL}/quoteList`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ quote }),
+    });
+    return response.status;
+}
+
+export const deleteQuote = async (id: number) => {
+    const response = await fetch(`${API_URL}/quoteList/${id}`, {
+        method: "DELETE",
+    });
+    return response.status;
+}
+
+export const getAllFavorites = async () => {
+    const response = await fetch(`${API_URL}/userFavoriteQuotes`);
+    return response.json();
+};
+
+export const addFavorite = async (favorite: Favorite) => {
+    const response = await fetch(`${API_URL}/userFavoriteQuotes`, {
+        method: "POST",
+        headers: {
+            "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ favorite }),
+    });
+    return response.status;
+}
+
+export const deleteFavorite = async (id: number) => {
+    const response = await fetch(`${API_URL}/userFavoriteQuotes/${id}`, {
+        method: "DELETE",
+    });
+    return response.status;
+}
 
