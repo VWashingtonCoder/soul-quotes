4170fd0bc6c4f7d5e62dead4861783d4d7d2b87b
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-10-15 13:02:01 -0600

    Build out html for category select

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,8 +1,43 @@
+const categories = [
+  { key: "all", label: "All Categories" },
+  { key: "inspirational", label: "Inspirational" },
+  { key: "love", label: "Love/Relationships" },
+  { key: "philosophy", label: "Philosophy" },
+  { key: "success", label: "Career/Success" },
+  { key: "funny", label: "Funny" },
+];
+
 function Home() {
   return (
-    <>
-      <h1>Soul Quotes - Home</h1>
-    </>
+    <section className="page home">
+      <header>
+        Welcome to Soul Quotes! This is a place to find inspiration and share
+        your own quotes.
+      </header>
+
+      <div className="category-select">
+        <label htmlFor="CategorySelect">Select a category:</label>
+        <select
+          name="category"
+          id="CategorySelect"
+          onChange={() => console.log("change")}
+        >
+          {categories.map((category) => (
+            <option key={category.key} value={category.key}>
+              {category.label}
+            </option>
+          ))}
+        </select>
+        <button
+          className="search-btn"
+          onClick={(e) => (e.preventDefault(), console.log("click"))}
+        >
+          Search
+        </button>
+      </div>
+
+      
+    </section>
   );
 }
 
