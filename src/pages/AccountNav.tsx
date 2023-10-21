2729ed5205f785b5b65fec3facd6364811df829d
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-10-20 19:12:35 -0600

    Style account-nav

--- src/pages/AccountNav.tsx
+++ src/pages/AccountNav.tsx
@@ -1,23 +1,25 @@
-import { Outlet, Link } from "react-router-dom";
+import { Outlet, Link, useLocation } from "react-router-dom";
 import { useUser } from "../context-hooks";
+import "../styles/Account.scss";
 
 function AccountNav() {
   const { activeUser } = useUser();
+  const location = useLocation().pathname;
   return (
-    <section className="page account-nav">
+    <section className="page account">
       {activeUser ? (
         <h2 className="loading-message">Taking You Home Now! Please wait...</h2>
       ) : (
         <>
-          <nav className="nav-links flex-between-center">
-            <ul>
+          <nav className="account-nav">
+            <ul className="nav-links flex-between-center">
               <li>
-                <Link to="/accounts/login" className="nav-link">
+                <Link to="/accounts/login" className={`nav-link ${location === "/accounts/login" && "active"}`}>
                   Login
                 </Link>
               </li>
               <li>
-                <Link to="/accounts/join" className="nav-link">
+                <Link to="/accounts/join" className={`nav-link ${location === "/accounts/join" && "active"}`}>
                   Join
                 </Link>
               </li>
