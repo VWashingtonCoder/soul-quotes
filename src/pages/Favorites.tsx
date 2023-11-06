d0dd4ff2c1e73be68684d2776c5190c2a56514a0
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-11-06 15:12:12 -0700

    Remove contributions table/view from favorites

--- src/pages/Favorites.tsx
+++ src/pages/Favorites.tsx
@@ -4,60 +4,32 @@ import CategorySelect from "../components/CategorySelect";
 import FavoritesTable from "../components/FavoritesTable";
 import "../styles/Favorites.scss";
 
-type TableViewType = "favorites" | "contributions";
-
 function Favorites() {
-  const { activeUser, activeUserFavorites } = useUser();
+  const { activeUserFavorites } = useUser();
   const { allQuotes } = useQuote();
-  const [tableView, setTableView] = useState("favorites" as TableViewType);
   const [favoritesCategory, setFavoritesCategory] = useState("all");
-  const [creationsCategory, setCreationsCategory] = useState("all");
   const userFavorites = allQuotes.filter((quote) =>
     activeUserFavorites.map((favorite) => favorite.qId).includes(quote.quoteId)
-  );
-  const userCreations = allQuotes.filter(
-    (quote) => quote.creatorId === activeUser?.userId
   );
   const favoritesList =
     favoritesCategory === "all"
       ? userFavorites
       : userFavorites.filter((quote) => quote.category === favoritesCategory);
-  const creationsList =
-    creationsCategory === "all"
-      ? userCreations
-      : userCreations.filter((quote) => quote.category === creationsCategory);
 
   return (
     <section className="page favorites">
-      <nav className="table-view-controls flex-between-center">
-        {["favorites", "contributions"].map((view) => (
-          <button
-            key={view}
-            className={`nav-link table-view-btn ${
-              tableView === view ? "active" : ""
-            }`}
-            onClick={() => setTableView(view as TableViewType)}
-          >
-            {view}
-          </button>
-        ))}
-      </nav>
+      <header className="favorites-header">
+        View all of your favorite quotes and edit their favorite status or you
+        can filter out your favorite quotes by quote category.
+      </header>
 
       <CategorySelect
-        searchCategory={
-          tableView === "favorites" ? favoritesCategory : creationsCategory
-        }
-        setSearchCategory={
-          tableView === "favorites"
-            ? setFavoritesCategory
-            : setCreationsCategory
-        }
+        searchCategory={favoritesCategory}
+        setSearchCategory={setFavoritesCategory}
         noSearch={true}
       />
 
-      <FavoritesTable
-        favoriteList={tableView === "favorites" ? favoritesList : creationsList}
-      />
+      <FavoritesTable favoriteList={favoritesList} />
     </section>
   );
 }
