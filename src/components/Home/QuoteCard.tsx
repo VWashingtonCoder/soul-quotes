06e4c81d5febc6b132ee8e820ab9b4374bffe0d3
V. Andre Washington <kuraiokamiblaze@gmail.com>
2023-11-17 12:36:00 -0700

    Update styling for better views

--- src/components/Home/QuoteCard.tsx
+++ src/components/Home/QuoteCard.tsx
@@ -27,44 +27,46 @@ const QuoteCard = (props: QuoteCardProps) => {
   const isCreator = activeUser?.userId === quote.creatorId;
 
   const handleQuoteDelete = (id: number) => {
-    removeQuote(id)
-    changeOneHomeQuote(idx)
+    removeQuote(id);
+    changeOneHomeQuote(idx);
   };
 
   return (
     <div className="quote-card" key={quoteId}>
       <div className="card-bar flex-between-center">
         <h3 className="card-category">{category}</h3>
-        <button
-          className="card-btn reload"
-          onClick={(e) => (e.preventDefault(), changeOneHomeQuote(idx))}
-        >
-          <TfiReload />
-        </button>
 
-        {activeUser && (
+        <div className="action-btns">
           <button
-            className="card-btn favorite"
-            onClick={(e) => {
-              e.preventDefault();
-              isFavorite
-                ? deleteFromFavorites(favoriteId)
-                : addToFavorites(quoteId);
-            }}
+            className="card-btn reload"
+            onClick={(e) => (e.preventDefault(), changeOneHomeQuote(idx))}
           >
-            {isFavorite ? <AiFillHeart /> : <AiOutlineHeart />}
+            <TfiReload />
           </button>
-        )}
-        {isCreator && (
-          <button
-            className="card-btn delete"
-            onClick={(e) => (
-              e.preventDefault(), handleQuoteDelete(quote.id as number)
-            )}
-          >
-            <AiFillDelete />
-          </button>
-        )}
+          {activeUser && (
+            <button
+              className="card-btn favorite"
+              onClick={(e) => {
+                e.preventDefault();
+                isFavorite
+                  ? deleteFromFavorites(favoriteId)
+                  : addToFavorites(quoteId);
+              }}
+            >
+              {isFavorite ? <AiFillHeart /> : <AiOutlineHeart />}
+            </button>
+          )}
+          {isCreator && (
+            <button
+              className="card-btn delete"
+              onClick={(e) => (
+                e.preventDefault(), handleQuoteDelete(quote.id as number)
+              )}
+            >
+              <AiFillDelete />
+            </button>
+          )}
+        </div>
       </div>
       <div className="card-body">
         <p className="card-content quote">{text}</p>
